295b680a4b84617dae7347c09ced3eb447006c07
aidan <aidanalrawi@icloud.com>
2024-07-13 01:26:50 -0400

    refactoring

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,8 @@
+
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+9
+	prizes.py,8/5/85514bcea8b98eafafbb8c5042604ce6f078ef24
+:
+
+prizes.csv,3/f/3fda787670f36be27c8852adfccdd32f8ea94100
--- prizes.py
+++ prizes.py
@@ -1,15 +1,22 @@
 import csv
 import sys
-import time
 
 
-def make_csv(final_payouts) -> None:
+def make_csv(final_payouts) -> bool:
     with open('prizes.csv', 'w', newline='') as csv_file:
         writer = csv.writer(csv_file)
         for key, value in final_payouts.items():
             writer.writerow([key, round(value, 2)])
 
     print("prizes.csv has been created!")
+    return True
+
+
+def get_total_prize_pool_for_alpha(alpha, top_prize, min_prize, winners) -> float:
+    total = 0
+    for i in range(1, winners + 1):
+        total += (top_prize - min_prize) / i ** alpha
+    return total
 
 
 def determine_a(prize_pool, top_prize, min_prize, winners) -> float:
@@ -17,66 +24,83 @@ def determine_a(prize_pool, top_prize, min_prize, winners) -> float:
     low = 0
     high = len(possibilities) - 1
 
-    left_eq = (prize_pool - (min_prize * winners))
+    money_to_be_allocated_to_larger_than_min_prize = (prize_pool - (min_prize * winners))
 
     while low < high:
         mid = int((low + high) // 2)
-        total = 0
-        for i in range(1, winners + 1):
-            total += (top_prize - min_prize) / i ** possibilities[mid]
+        total_prize_pool = get_total_prize_pool_for_alpha(possibilities[mid], top_prize, min_prize, winners)
 
-        if left_eq - 0.01 * total <= total <= left_eq + 0.01 * total:
+        if money_to_be_allocated_to_larger_than_min_prize - 0.01 * total_prize_pool <= total_prize_pool <= money_to_be_allocated_to_larger_than_min_prize + 0.01 * total_prize_pool:
             print("Alpha Found")
             return possibilities[mid]
-        elif left_eq - 0.01 < total:
+
+        elif money_to_be_allocated_to_larger_than_min_prize - 0.01 < total_prize_pool:
             low = mid + 1
-        elif left_eq + 0.01 > total:
+
+        elif money_to_be_allocated_to_larger_than_min_prize + 0.01 > total_prize_pool:
             high = mid - 1
 
     if low == high:
         print("Approx Alpha Found")
         return possibilities[mid]
-    else:
-        print("not Found")
+
+    print("not Found")
 
 
-def build_payouts(top_prize, min_prize, winners, alpha) -> dict:
-    keys = list(range(1, winners + 1))
-    output = {k: (min_prize + ((top_prize - min_prize) / (k ** alpha))) for k in keys}
+def get_payout_for_place(place, top_prize, min_prize, alpha) -> float:
+    return min_prize + ((top_prize - min_prize) / (place ** alpha))
+
+
+def build_payouts_dictionary(top_prize, min_prize, winners, alpha) -> dict:
+    placing_list = list(range(1, winners + 1))
+    output = {placing: get_payout_for_place(placing, top_prize, min_prize, alpha) for placing in placing_list}
     return output
 
 
-def main():
+def interactive_input():
     participants = int(input('Please enter the number of participants: '))
-    prize_pool = round(0.05 * participants, 5)
-    print("Prize Pool: " + str(prize_pool))
-    winners = round(participants * 0.2)
+    prize_pool = round(0.05 * participants, 4)
+    print(f"Prize Pool: {prize_pool}")
 
-    top_prize = input("Enter custom first place in EUR. Leave blank for default 15% of prize pool = "
-                      + str(prize_pool * 0.15) + "EUR: ")
-    if not top_prize:
-        top_prize = 0.15 * prize_pool
-    else:
-        top_prize = float(top_prize)
+    top_prize_payout = round(0.15 * prize_pool, 4)
+    top_prize_input = input(
+        f"Enter custom first place in EUR. Leave blank for default 15% of prize pool = {top_prize_payout}:")
+    top_prize = 0.15 * prize_pool
+    if top_prize_input:
+        top_prize = float(top_prize_input)
 
-    min_prize = input("Enter custom min prize in EUR. Leave blank for default 0.01 EUR: ")
-    if not min_prize:
-        min_prize = 0.01
-    else:
-        min_prize = float(min_prize)
+    min_prize = 0.01
+    min_prize_input = input("Enter custom min prize. Leave blank for default 0.01: ")
+    if min_prize_input:
+        min_prize = float(min_prize_input)
 
-    alpha = determine_a(prize_pool=prize_pool, top_prize=top_prize, min_prize=min_prize,winners=winners)
-    make_csv(build_payouts(top_prize=top_prize, min_prize=min_prize, winners=winners, alpha=alpha))
+    number_of_winners = round(participants * 0.2)
+    winners_prompt = f"Enter custom number of winners. Leave blank for default 20% of participants = {number_of_winners}:"
+    winners_input = input(winners_prompt)
+    if winners_input:
+        number_of_winners = int(winners_input)
 
-    seconds_left = 5
-    while seconds_left:
-        sys.stdout.write("\r")
-        sys.stdout.write(f"Closing in {seconds_left}")
-        time.sleep(1)
-        seconds_left -= 1
-        sys.stdout.flush()
+    result_dict = {
+        "top_prize": top_prize,
+        "min_prize": min_prize,
+        "winners": number_of_winners,
+        "prize_pool": prize_pool
+    }
 
-    sys.stdout.write("\r")
+    return result_dict
+
+
+def main():
+    user_input = interactive_input()
+    top_prize = user_input["top_prize"]
+    min_prize = user_input["min_prize"]
+    winners = user_input["winners"]
+    prize_pool = user_input["prize_pool"]
+
+    alpha = determine_a(prize_pool=prize_pool, top_prize=top_prize, min_prize=min_prize, winners=winners)
+    payouts_dict = build_payouts_dictionary(top_prize=top_prize, min_prize=min_prize, winners=winners, alpha=alpha)
+    make_csv(payouts_dict)
+
     sys.stdout.write("Goodbye!")
     sys.exit(0)
 
